e191a32e4bf4e46839dddcb19a1fb594ed019041
Tigro <easterlingme95@gmail.com>
2023-02-28 21:10:52 -0600

    added workbox plugins and manifest file

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -18,7 +18,28 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-
+      new InjectManifest({
+        swSrc: './src-sw.js',
+        swDest: 'src-sw.js',
+      }),
+      new WebpackPwaManifest({
+        fingerprints: false,
+        inject: true,
+        name: 'text editor',
+        short_name: 'JATE',
+        description: 'Never forget your text!',
+        background_color: '#225ca3',
+        theme_color: '#225ca3',
+        start_url: './',
+        publicPath: './',
+        icons: [
+          {
+            src: path.resolve('src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons'),
+          },
+        ],
+      }),
     ],
 
     module: {
